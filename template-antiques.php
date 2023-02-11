ea871b6c2ad39b123b4a1521856a4fed4e2aa772
Pashinsky Mikhail <mpashinsky@gmail.com>
2023-02-11 10:58:20 +0300

    Add echo for test

--- template-antiques.php
+++ template-antiques.php
@@ -79,6 +79,7 @@ $translate['categories'] 	= mfn_opts_get('translate') ? mfn_opts_get('translate-
                     $slider = new RevSlider();
                     $revolution_sliders = $slider->getAllSliderAliases();
                     foreach( $revolution_sliders as $revolution_slider ) {
+                        echo $revolution_slider;
                         if( $revolution_slider == $shortcode ) {
                                 echo $revolution_slider;
                              return true;
