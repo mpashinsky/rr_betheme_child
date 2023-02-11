b9fbb6ab1b8ee8f9c8d5757f357e9c9e722dc08e
Pashinsky Mikhail <mpashinsky@gmail.com>
2023-02-11 11:14:57 +0300

    Fixed error

--- template-antiques.php
+++ template-antiques.php
@@ -79,9 +79,9 @@ $translate['categories'] 	= mfn_opts_get('translate') ? mfn_opts_get('translate-
                     $slider = new RevSlider();
                     $revolution_sliders = $slider->get_sliders();
                     foreach( $revolution_sliders as $revolution_slider ) {
-                        echo $revolution_slider;
+                        echo $revolution_slider->alias;
                         if( $revolution_slider->alias == $alias ) {
-                                echo $revolution_slider;
+                              echo $revolution_slider->alias;
                              return true;
                         }
                     }
