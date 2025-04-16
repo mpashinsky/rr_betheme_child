34d5e4b49599ba9f4d48d47a840b90310c6f205a
Pashinsky Mikhail <mpashinsky@gmail.com>
2025-04-16 23:56:18 +0300

    Conditional echo of variable

--- search.php
+++ search.php
@@ -78,7 +78,9 @@ $translate['categories'] 	= mfn_opts_get('translate') ? mfn_opts_get('translate-
 								<?php
 
 								echo '<ul class="portfolio_group lm_wrapper isotope '. $portfolio_classes .'">';
-							 		echo romanov_antiques_archive( $portfolio_query );
+                                if (isset($portfolio_query)) {
+                                    echo romanov_antiques_archive($portfolio_query);
+                                }
 								echo '</ul>';
 								
 									/*
