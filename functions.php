4cc9b96b3d9af76da6b20006609c7a7c744e88fe
Pashinsky Mikhail <mpashinsky@gmail.com>
2025-10-25 22:18:46 +0300

    rollback slider changes

--- functions.php
+++ functions.php
@@ -844,10 +844,54 @@ if ( ! function_exists( 'responsive_related_posts' ) ) :
 				$output .= '</div>';
                 $output .= '<script>
                     jQuery(document).ready(function($){
-                            var $slider = $(".' . $class_to_apply . '");
-                            $slider.slick();
-                    });
-                </script>';
+  							$(".' . $class_to_apply . '").slick({
+    							slidesToShow : ' . $slides_to_show . ',
+								slidesToScroll : 1,
+                                arrows: true,
+								prevArrow : \'<button type="button" class="slick-prev">&lt;</button>\',
+								nextArrow : \'<button type="button" class="slick-next">&gt;</button>\',
+								responsive: [
+								responsive: [
+								    {
+								        breakpoint: 1024,
+								        settings: {
+								        slidesToShow: 3,
+								        slidesToScroll: 1
+								      }
+								    },
+                                    {
+                                      breakpoint:769,
+                                      settings: {
+                                        slidesToShow: 2,
+                                        slidesToScroll: 1
+                                      }
+                                    },
+                                    {
+                                      breakpoint:767,
+                                      settings: {
+                                        slidesToShow: 1,
+                                        slidesToScroll: 1
+                                      }
+                                    },
+								    {
+								      breakpoint: 600,
+								      settings: {
+								        slidesToShow: 1,
+								        slidesToScroll: 1
+								      }
+								    },
+								    {
+								      breakpoint: 480,
+								      settings: {
+								        slidesToShow: 1,
+								        slidesToScroll: 1
+								      }
+								    }
+								  ]
+								]
+  							});
+						});
+					</script>';
             echo $output;
 
             ?>
